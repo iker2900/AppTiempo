1d66201c99f1417b43c2fa45e17c528a509b6548
iker2900 <iker29992@gmail.com>
2026-01-28 21:26:35 +0100

    style: apply iOS-like dark theme and glassmorphism

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,14 @@ import { RouteReuseStrategy } from '@angular/router';
 import {
   provideHttpClient,
   withInterceptorsFromDi,
-  HttpClient,
 } from '@angular/common/http';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateModule } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
-export function createTranslateLoader(http: HttpClient) {
-  return new TranslateHttpLoader(http);
-}
 
 @NgModule({
   declarations: [AppComponent],
@@ -25,17 +20,13 @@ export function createTranslateLoader(http: HttpClient) {
     IonicModule.forRoot(),
     AppRoutingModule,
     TranslateModule.forRoot({
-      defaultLanguage: 'es',
-      loader: {
-        provide: TranslateLoader,
-        useFactory: createTranslateLoader,
-        deps: [HttpClient],
-      },
+      defaultLanguage: 'es'
     }),
   ],
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideHttpClient(withInterceptorsFromDi()),
+    provideTranslateHttpLoader(),
   ],
   bootstrap: [AppComponent],
 })
