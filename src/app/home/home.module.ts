bceccb24a17abf4ed289346a7836f793b2e2f4d5
iker2900 <iker29992@gmail.com>
2026-01-28 20:54:25 +0100

    feat: implement home page logic (integrated view)

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,17 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
+import { TranslateModule } from '@ngx-translate/core';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
-
+import { SearchFormComponent } from '../design/organisms/search-form/search-form.component';
+import { CurrentWeatherComponent } from '../design/organisms/current-weather/current-weather.component';
+import { ForecastListComponent } from '../design/organisms/forecast-list/forecast-list.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    TranslateModule,
+    SearchFormComponent,
+    CurrentWeatherComponent,
+    ForecastListComponent
   ],
   declarations: [HomePage]
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { WeatherService } from '../core/services/weather.service';
+import { LocationService } from '../core/services/location.service';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +10,24 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  currentWeather = this.weatherService.currentWeather;
+  forecast = this.weatherService.forecast;
 
+  constructor(
+    private weatherService: WeatherService,
+    private locationService: LocationService
+  ) {}
+
+  onSearch(city: string) {
+    this.weatherService.getCurrentWeather(city).subscribe();
+    this.weatherService.getForecast(city).subscribe();
+  }
+
+  async onUseLocation() {
+    const pos = await this.locationService.getCurrentPosition();
+    if (pos) {
+      this.weatherService.getWeatherByCoords(pos.coords.latitude, pos.coords.longitude).subscribe();
+      this.weatherService.getForecastByCoords(pos.coords.latitude, pos.coords.longitude).subscribe();
+    }
+  }
 }
